c144f55a762d81175e5795b487cf193aeeb83587
jay-java <jayprajapati.tops@gmail.com>
2025-12-22 09:11:23 +0530

    selenium browser configuration

---
+++ Selenium_Learning/src/test/java/com/selenium/P001_OpenBrowser.java
@@ -0,0 +1,38 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class P001_OpenBrowser {
+	public static void main(String[] args) throws InterruptedException {
+		// 1.Chrome
+//		System.setProperty("webdriver.chrome.driver",
+//				"C:\\Users\\Admin\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//
+//		driver.get("https://www.facebook.com/");
+//		driver.manage().window().maximize();
+//
+//		Thread.sleep(3000);
+//		driver.close();
+
+		// 2.Firefox
+//		System.setProperty("webdriver.gecko.driver",
+//				"C:\\Users\\Admin\\Downloads\\geckodriver-v0.36.0-win64\\geckodriver.exe");
+//		WebDriver driver = new FirefoxDriver();
+//		driver.get("https://www.facebook.com/");
+//		driver.manage().window().maximize();
+//		Thread.sleep(3000);
+//		driver.close();
+
+		// 3.Edge
+		System.setProperty("webdriver.edge.driver", "C:\\Users\\Admin\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.close();
+	}
+}
